a574eea6d5a6ef965885d1e72cd2da53da5ed398
Citlalli Del Moral <citlallidmg@gmail.com>
2022-02-27 19:30:01 -0600

    update: adds favicon and login page

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0d0d0d",
+      light: "#a3a3af",
+      dark: "#090909",
+    },
+    secondary: {
+      main: "#f04e49",
+      light: "#f46e70",
+      dark: "#a93335",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
