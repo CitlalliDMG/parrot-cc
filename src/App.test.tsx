42405650fd1edc94091e7945f73d4fc6837ce406
Citlalli Del Moral <citlallidmg@gmail.com>
2022-02-28 06:45:26 -0600

    update: fixes App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,30 @@
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import configureStore from 'redux-mock-store';
 import App from './App';
 
+const sagaMiddleware = createSagaMiddleware();
+const mockStore = configureStore([sagaMiddleware]);
+
 test('renders App', () => {
-    render(<App />);
+    const store = mockStore({
+        login: {
+            data: {},
+            error: {},
+        },
+        data: {
+            store: {},
+            products: {},
+            error: {},
+        },
+        error: {
+            status: false,
+        },
+    });
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
 });
